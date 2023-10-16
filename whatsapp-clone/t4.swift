11f43468aca6165b3f15f44e9041564b11811f1c
Ngori Januário <ngorijanuario@gmail.com>
2023-10-16 08:40:13 +0100

    Criação da t4

---
+++ whatsapp-clone/t4.swift
@@ -0,0 +1,22 @@
+//
+//  t4.swift
+//  whatsapp-clone
+//
+//  Created by Ngori Januario on 15/10/23.
+//
+
+import SwiftUI
+
+struct t4: View {
+    var body: some View {
+        NavigationView {
+            Text("")
+            
+            .navigationTitle("Conversas")
+        }
+    }
+}
+
+#Preview {
+    t4()
+}

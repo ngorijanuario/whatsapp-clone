3c257f7dc3d2e4aae947c0a1ab3caa4cf02c4436
Ngori Januário <ngorijanuario@gmail.com>
2023-10-16 08:39:54 +0100

    Criação da t2

---
+++ whatsapp-clone/t2.swift
@@ -0,0 +1,22 @@
+//
+//  t2.swift
+//  whatsapp-clone
+//
+//  Created by Ngori Januario on 15/10/23.
+//
+
+import SwiftUI
+
+struct t2: View {
+    var body: some View {
+        NavigationView {
+            Text("")
+            
+            .navigationTitle("Chamadas")
+        }
+    }
+}
+
+#Preview {
+    t2()
+}

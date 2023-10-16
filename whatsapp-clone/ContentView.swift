a84c0266b50469d94400e88dea14687f94f5ab3a
Ngori Januário <ngorijanuario@gmail.com>
2023-10-16 08:38:28 +0100

    Criação das telas

--- whatsapp-clone/ContentView.swift
+++ whatsapp-clone/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView{
+            t1()
+                .tabItem{
+                    Image(systemName: "person.2.gobackward")
+                    Text("Atualizações")
+                }
+            t2()
+                .tabItem{
+                    Image(systemName: "phone")
+                    Text("Chamadas")
+                }
+            t3()
+                .tabItem{
+                    Image(systemName: "person.3")
+                    Text("Comunidades")
+                }
+            t4()
+                .tabItem{
+                    Image(systemName: "message.fill")
+                    Text("Conversas")
+                }
+            t5()
+                .tabItem{
+                    Image(systemName: "gear")
+                    Text("Definiçoões")
+                }
+                        
+            }
     }
 }
 

fe82c596be8cb1b47dbdcc3952416e0efe1f7367
Ngori Januário <ngorijanuario@gmail.com>
2023-10-16 08:40:04 +0100

    Criação da t3

---
+++ whatsapp-clone/t3.swift
@@ -0,0 +1,22 @@
+//
+//  t3.swift
+//  whatsapp-clone
+//
+//  Created by Ngori Januario on 15/10/23.
+//
+
+import SwiftUI
+
+struct t3: View {
+    var body: some View {
+        NavigationView {
+            Text("")
+            
+            .navigationTitle("Comunidades")
+        }
+    }
+}
+
+#Preview {
+    t3()
+}

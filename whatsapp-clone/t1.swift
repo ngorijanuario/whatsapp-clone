a63b0685957f414e1a2bdc2757c9db706e0d713e
Ngori Januário <ngorijanuario@gmail.com>
2023-10-16 08:39:38 +0100

    Criação da t1

---
+++ whatsapp-clone/t1.swift
@@ -0,0 +1,22 @@
+//
+//  t1.swift
+//  whatsapp-clone
+//
+//  Created by Ngori Januario on 15/10/23.
+//
+
+import SwiftUI
+
+struct t1: View {
+    var body: some View {
+        NavigationView {
+            Text("")
+            
+            .navigationTitle("Atualicações")
+        }
+    }
+}
+
+#Preview {
+    t1()
+}
